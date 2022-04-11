6d373567c12f6445d6f8c93c2b430201947e8f6e
matikoblinc <78440014+matikoblinc@users.noreply.github.com>
2022-04-11 19:39:39 -0300

    Exercise 4 finished

--- Assets/Scripts/Ej03.cs
+++ Assets/Scripts/Ej03.cs
@@ -4,10 +4,42 @@ using UnityEngine;
 
 public class Ej03 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    const int HORA_APERTURA = 10;
+    const int HORA_CIERRE = 18;
+    public int hora;
+    bool estaAbierto;
+
     void Start()
     {
-        
+        if (hora < 0 || hora > 24)
+        {
+            Debug.Log("la hora es incorrecta");
+        }
+        else
+        {
+
+
+            if (hora > HORA_APERTURA && hora < HORA_CIERRE)
+            {
+                estaAbierto = true;
+            }
+            else
+            {
+                estaAbierto = false;
+            }
+
+           switch (estaAbierto)
+            {
+                case true:
+                    Debug.Log("el estacionamiento está abierto");
+                    break;
+
+                case false:
+                    Debug.Log("el estacionamiento está cerrado");
+                    break;
+            }
+
+        }
     }
 
     // Update is called once per frame
@@ -16,3 +48,11 @@ public class Ej03 : MonoBehaviour
         
     }
 }
+//10. Realizá un programa que resuelva el siguiente problema: Nos piden desarrollar un  programa que informe si
+//un estacionamiento se encuentra o no abierto según la hora  ingresada por el usuario.  
+//Nosotros sabemos que el estacionamiento se encuentra abierto entre las 10 y las 18hs.
+//(Crear constante HORA_APERTURA = 10, y HORA_CIERRE = 18).  
+//Al iniciar el programa, se tomará como hora actual el valor de una variable expuesta en el Inspector,
+//y deberemos setear una  variable booleana llamada estaAbierto en true o false, según corresponda, e informar por
+//pantalla mediante la misma si el estacionamiento se encuentra abierto o no. Obs: En el caso de que el valor de la variable
+//correspona a una hora menor a 0 o mayor a 24, mostrar el  mensaje: “Ha ingresado una hora incorrecta” 
---
+++ Assets/Scripts/Ej04.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej04 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    public int num3;
+
+    void Start()
+    {
+        if (num1 == num2 && num2 == num3)
+        {
+            Debug.Log("son todos iguales");
+        }
+        else if (num1 > num2 && num1 > num3)
+        {
+            Debug.Log("el mayor es num1");
+        }
+        else if (num2 > num1 && num2 > num3)
+        {
+            Debug.Log("el mayor es num2");
+        }
+        else if (num3 > num2 && num3 > num1)
+        {
+            Debug.Log("el mayor es num3");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+//7. Realizá un programa que al ingresar por Inspector tres números enteros num1, num2 y  num3, muestre el valor del mayor de ellos. 
+//En caso de igualdad entre los tres imprimir "Los números son iguales".  Test1: num1 = 12, num2 = 4, num3 = 7.Resultado esperado:
+//El mayor número es num1  Test2: num1 = 2, num2 = 65, num3 = 8.Resultado esperado: El mayor número es num2 
+//Test3: num1 = 3, num2 = 10, num3 = 28.Resultado esperado: El mayor número es num3  Test4: num1 = 5, num2 = 5, num3 = 5.Resultado
+//esperado: Los números son iguales
